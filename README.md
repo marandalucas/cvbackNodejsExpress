e3b69e8056b83f76a784c0b079fc9a4fedf046fb
marcos.aranda <marcos.aranda@bbva.com>
2020-06-14 20:43:18 +0200

    FEAT: Implemented get projects by category

---
+++ README.md
@@ -0,0 +1,21 @@
+## CV Backend Nodejs Express
+
+Technologies:
+
+- nodejs with Expres
+- jwt-simple
+- moment
+- cors
+- mongoose
+- heroku
+
+## How to start up
+
+- Install dependencies
+```bash
+$ npm install
+```
+- Run project
+```bash
+$ npm run startdev
+```
--- routes/api/proyectos.js
+++ routes/api/proyectos.js
@@ -14,12 +14,23 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:categoria', async (req, res) => {
+    try {
+        const proyectos = await Proyecto.find({ categoria: req.params.categoria })
+        res.json(proyectos); 
+    } catch (err) {
+        res.status(503).json({ 'error': err });
+    }
+});
+
 router.post('/', [
     check('titulo', 'El titulo debe incluirse en la peticion y tiene un máximo de 40 caracteres')
         .exists()
+        .notEmpty()
         .isLength({ max: 40 }),
     check('descripcion', 'El descripcion debe incluirse en la peticion y tiene un máximo de 300 caracteres')
         .exists()
+        .notEmpty()
         .isLength({ max: 300 }),
     check('url', 'La url del proyecto debe estar correcta')
         .isURL()
