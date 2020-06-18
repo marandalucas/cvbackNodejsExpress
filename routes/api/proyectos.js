8ac4d6820166a10c5f76fa2855b7b6ea69d9e32c
marcos.aranda <marcos.aranda@bbva.com>
2020-06-18 21:24:41 +0200

    FEAT: New API resources Get Project By Category and Views

--- routes/api/proyectos.js
+++ routes/api/proyectos.js
@@ -14,14 +14,24 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:categoria', async (req, res) => {
+
+router.get('categoria/:categoria', async (req, res) => {
     try {
         const proyectos = await Proyecto.find({ categoria: req.params.categoria })
-        res.json(proyectos); 
+        res.json(proyectos);
     } catch (err) {
         res.status(503).json({ 'error': err });
     }
 });
+
+router.get('/:idProyecto', async (req, res) => {
+    try {
+        const proyecto = await Proyecto.findById(req.params.idProyecto);
+        res.json(proyecto);
+    } catch {
+        res.status(503).json({ 'error': err });
+    }
+})
 
 router.post('/', [
     check('titulo', 'El titulo debe incluirse en la peticion y tiene un máximo de 40 caracteres')
